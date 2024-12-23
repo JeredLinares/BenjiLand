8510637ca546e83ca8cfcc35060c7aec580782c9
JD Linares <JeredL@gmail.com>
2024-12-23 02:41:48 +0000

    iternation function working as shown by debugger output

--- 01_benji_game_of_life_v0.1/benji_game_of_life.py
+++ 01_benji_game_of_life_v0.1/benji_game_of_life.py
@@ -13,6 +13,25 @@
 import RPi.GPIO as GPIO     # Depends on Raspberry Pi Zero W's GPIO Pins
 #import numpy as np         # Practicing Python, not numpy
 
+'''
+# GPIOs have default values that you override
+#GPIO.setwarnings(False)
+
+# Initalize GPIO Pins
+GPIO.setmode(GPIO.BCM)
+# LEDs
+# BCM board does not use numbers 17-20
+led_list_a = list(range(1,17))
+led_list_b = list(range(21,30))
+led_list = led_list_a + led_list_b
+GPIO.setup(led_list,GPIO.OUT)
+GPIO.output(led_list,GPIO.LOW)
+
+# Buttons
+button_list = list(range(30,32))
+GPIO.setup(button_list,GPIO.IN)
+
+'''
 
 # Initalize a global 5x5x17 matrix which will hold the starting conditions
 # Account for Python's copy by reference (ie. don't use [[[0]*n]*n]*n])
@@ -26,20 +45,25 @@ game_starting_conditions = [[[0 for i in range(5)] for j in range(5)] for k in r
 start_counts = 0
 
 
+
 # Read starting frames file and set values in starting frames matrix
 with open("./input.txt") as starting_frames_data:
     starting_frames_data.readline()                     # Clear description
+#    print(starting_frames_data)
     number_lines = starting_frames_data.readline()      # Number of starting frames, always of size 5x5
+#    print(number_lines)
     for frame in range(int(number_lines)):
-#        print(starting_frames_data.readline())          # Name of starting frame
+        frame_name = starting_frames_data.readline()
+#        print(frame_name)          # Name of starting frame
         for line_number in range(5):
             line_data = starting_frames_data.readline()             # Line in starting frame
             line_to_set = line_data.strip().split(',')
             line_to_set = [int(item) for item in line_to_set]
             game_starting_conditions[frame][line_number] = line_to_set
-
 #print(game_starting_conditions)
-#print(len(game_starting_conditions))
+
+# Initalize a 5x5 matrix to use as current frame
+current_frame = game_starting_conditions[0]
 
 
 def next_frame():
@@ -47,21 +71,63 @@ def next_frame():
     # Function to Iterate to next frame
     # Due to 5x5 constraint due to limited GPIO pins on Rasberry pi, act as if continous column 0 to column 4 and row 0 to row 4 (
     # Rule 1:  Any live cell with fewer than 2 live neighbours dies, as if by under population
-    # Rule 2:  Any life cell with 2 or 3 live neighbours lives on to the next generation
-    # Rule 3:  Any live cell with more than 3 live neighbours dies, as if by under population
+    # Rule 2:  Any live cell with 2 or 3 live neighbours lives on to the next generation
+    # Rule 3:  Any live cell with more than 3 live neighbours dies, as if by over population
     # Rule 4:  Any dead cell with exactly 3 live neighbours becomes a live cell, as if by reproduction
     """
 
+    # Define a 5x5 array to return as the next frame
+    next_frame = [[0 for x in range(5)] for y in range(5)]
 
+    #iterate though next_frame calculating births/deaths/stays
+    for row in range(5):
+        for element in range(5):
+            # Count number of alive cells around the new cell from the current frame and store in next_frame
+            number_alive_cells = 0
+            for x in range(-1,2):
+                for y in range(-1,2):
+                    if( x!=0 or y!=0 ):    #this has to be complicated because python doesn't let you leave empty if statements
+                        number_alive_cells += current_frame[ (row + x) % 5 ][ (element + y) % 5 ]
+            if number_alive_cells < 2:
+                next_frame[row][element] = 0
+            elif number_alive_cells > 3:
+                next_frame[row][element] = 0
+            elif number_alive_cells == 2:
+                next_frame[row][element] = current_frame[row][element]
+            elif number_alive_cells == 3: 
+                next_frame[row][element] = 1
+    return next_frame
+
+
+# For Debugging: iterate though 10 steps of each starting case
+print("Debugger output")
+for starter in range(17):
+    current_frame = game_starting_conditions[starter]
+    print()
+    print()
+    print("Restart " + str(starter))
+    [print(output_row) for output_row in current_frame]
+    print()
+    for step in range(10):
+        current_frame = next_frame()
+        [print(output_row) for output_row in current_frame]
+        print()
 
 
 
 # Loop checking if eather button is pressed
 
 
+# GPIO.input(30)
+# GPIO.input(31)
 
 
-# print("hello world")
+
+# Should never reach this point due to infinate loop
+
+
+
+
 
 
 
